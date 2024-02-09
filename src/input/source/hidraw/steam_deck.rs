75f2ace1eb59abe3b73b6147b96592d681a72fad
William Edwards <shadowapex@gmail.com>
2024-02-09 01:13:52 -0800

    Read the proper hidraw device

--- src/input/source/hidraw/steam_deck.rs
+++ src/input/source/hidraw/steam_deck.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 
 use hidapi::DeviceInfo;
 use packed_struct::PackedStruct;
+use std::ffi::CString;
 use tokio::sync::broadcast;
 
 use crate::{
@@ -31,23 +32,22 @@ impl DeckController {
 
     pub async fn run(&self) -> Result<(), Box<dyn Error>> {
         log::debug!("Starting Steam Deck Controller driver");
-        let api = hidapi::HidApi::new()?;
-        let device = api.open(VID, PID)?;
+        let path = CString::new(self.info.path().to_string_lossy().to_string()).unwrap();
 
         // Spawn a blocking task to read the events
         let task = tokio::task::spawn_blocking(
             move || -> Result<(), Box<dyn Error + Send + Sync>> {
+                let api = hidapi::HidApi::new()?;
+                let device = api.open_path(&path)?;
                 loop {
                     // Read data from the device into a buffer
-                    let mut buf = [0u8; PACKET_SIZE];
+                    let mut buf = [0; PACKET_SIZE];
                     let bytes_read = device.read_timeout(&mut buf[..], HID_TIMEOUT)?;
 
                     // All report descriptors are 64 bytes, so this is just to be safe
                     if bytes_read != PACKET_SIZE {
-                        //let msg = format!("Invalid input report size was received from gamepad device: {bytes_read}/{PACKET_SIZE}");
-                        log::debug!("Invalid input report size was received from gamepad device: {bytes_read}/{PACKET_SIZE}");
-                        continue;
-                        //return Err(msg.into());
+                        let msg = format!("Invalid input report size was received from gamepad device: {bytes_read}/{PACKET_SIZE}");
+                        return Err(msg.into());
                     }
 
                     // Unpack the input report
