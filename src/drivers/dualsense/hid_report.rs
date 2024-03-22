9aae95116a02b483852816702058e39dcfcddf29
Denis Benato <benato.denis96@gmail.com>
2024-03-22 02:19:43 +0100

    WIP: dualsense

--- src/drivers/dualsense/hid_report.rs
+++ src/drivers/dualsense/hid_report.rs
@@ -1,21 +1,49 @@
-use packed_struct::{types::SizedInteger, PackedStruct};
+use packed_struct::{prelude::*, types::SizedInteger, PackedStruct};
+
+use super::driver::*;
+
+pub trait PackedInputDataReport: PackedStruct + Copy + Clone + PartialEq
+{
+    fn new() -> Self;
+
+    fn set_frame_number(&mut self, num: u8);
+} 
 
 #[derive(PackedStruct, Debug, Copy, Clone, PartialEq)]
 #[packed_struct(bit_numbering = "msb0", size_bytes = "64")]
-pub struct PackedInputDataReport {
-    
+pub struct USBPackedInputDataReport {
+    #[packed_field(bytes = "0")]
+    pub input_report: u8,
+
+    #[packed_field(bytes = "1")]
+    pub joystick_l_x: u8,
+    #[packed_field(bytes = "2")]
+    pub joystick_l_y: u8,
+    #[packed_field(bytes = "3")]
+    pub joystick_r_x: u8,
+    #[packed_field(bytes = "4")]
+    pub joystick_r_y: u8,
+
 }
 
-impl PackedInputDataReport {
+impl PackedInputDataReport for USBPackedInputDataReport {
     /// Return a new empty input data report
-    pub fn new() -> Self {
-        PackedInputDataReport {
-           
+    fn new() -> Self {
+        Self {
+            input_report: INPUT_REPORT_USB,
+            joystick_l_x : 127,
+            joystick_l_y : 127,
+            joystick_r_x : 127,
+            joystick_r_y : 127,
         }
+    }
+
+    fn set_frame_number(&mut self, num: u8) {
+
     }
 }
 
-impl Default for PackedInputDataReport {
+impl Default for USBPackedInputDataReport {
     fn default() -> Self {
         Self::new()
     }
