7f3356d88b8cc027db48fc7376878a10ddeb8907
William Edwards <shadowapex@gmail.com>
2024-08-26 13:37:54 -0700

    feat(Evdev): expose source device capabilities over dbus

--- src/dbus/interface/source/evdev.rs
+++ src/dbus/interface/source/evdev.rs
@@ -1,20 +1,33 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
+use evdev::EventType;
 use zbus::{fdo, Connection};
 use zbus_macros::interface;
 
-use crate::{input::source::evdev::get_dbus_path, udev::device::UdevDevice};
+use crate::{
+    input::source::evdev::{get_capabilities, get_dbus_path},
+    udev::device::UdevDevice,
+};
 
 /// The [SourceEventDeviceInterface] provides a DBus interface that can be exposed for managing
 /// a [Manager]. It works by sending command messages to a channel that the
 /// [Manager] is listening on.
 pub struct SourceEventDeviceInterface {
     device: UdevDevice,
+    capabilities: HashMap<EventType, Vec<u16>>,
 }
 
 impl SourceEventDeviceInterface {
     pub fn new(device: UdevDevice) -> SourceEventDeviceInterface {
-        SourceEventDeviceInterface { device }
+        let handler = device.devnode();
+        let capabilities = get_capabilities(handler.as_str()).unwrap_or_else(|e| {
+            log::warn!("Failed to get capabilities for source evdev device '{handler}': {e:?}");
+            HashMap::new()
+        });
+        SourceEventDeviceInterface {
+            device,
+            capabilities,
+        }
     }
 
     /// Creates a new instance of the source evdev interface on DBus. Returns
@@ -39,6 +52,15 @@ impl SourceEventDeviceInterface {
             }
         });
         Ok(())
+    }
+
+    /// Returns all the event codes for the given event type that this evdev device
+    /// supports.
+    pub fn supported_events(&self, event_type: &EventType) -> Vec<u16> {
+        self.capabilities
+            .get(event_type)
+            .map(|caps| caps.to_owned())
+            .unwrap_or_default()
     }
 }
 
@@ -96,5 +118,64 @@ impl SourceEventDeviceInterface {
     #[zbus(property)]
     async fn unique_id(&self) -> fdo::Result<String> {
         Ok(self.device.uniq())
+    }
+
+    /// Returns the set of supported keys reported by the device.
+    ///
+    /// For keyboards, this is the set of all possible keycodes the keyboard may emit. Controllers,
+    /// mice, and other peripherals may also report buttons as keys.
+    #[zbus(property)]
+    async fn supported_keys(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::KEY))
+    }
+
+    /// Returns the set of supported "relative axes" reported by the device.
+    ///
+    /// Standard mice will generally report `REL_X` and `REL_Y` along with wheel if supported.
+    #[zbus(property)]
+    async fn supported_relative_axes(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::RELATIVE))
+    }
+
+    /// Returns the set of supported "absolute axes" reported by the device.
+    ///
+    /// These are most typically supported by joysticks and touchpads.
+    #[zbus(property)]
+    async fn supported_absolute_axes(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::ABSOLUTE))
+    }
+
+    /// Returns the set of supported switches reported by the device.
+    ///
+    /// These are typically used for things like software switches on laptop lids (which the
+    /// system reacts to by suspending or locking), or virtual switches to indicate whether a
+    /// headphone jack is plugged in (used to disable external speakers).
+    #[zbus(property)]
+    async fn supported_switches(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::SWITCH))
+    }
+
+    /// Returns a set of supported LEDs on the device.
+    ///
+    /// Most commonly these are state indicator lights for things like Scroll Lock, but they
+    /// can also be found in cameras and other devices.
+    #[zbus(property)]
+    async fn supported_leds(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::LED))
+    }
+
+    /// Returns the set of supported simple sounds supported by a device.
+    ///
+    /// You can use these to make really annoying beep sounds come from an internal self-test
+    /// speaker, for instance.
+    #[zbus(property)]
+    async fn supported_sounds(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::SOUND))
+    }
+
+    /// Returns the set of supported force feedback effects supported by a device.
+    #[zbus(property)]
+    async fn supported_ff(&self) -> fdo::Result<Vec<u16>> {
+        Ok(self.supported_events(&EventType::FORCEFEEDBACK))
     }
 }
