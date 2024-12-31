54db7ae2c70af10e1fe4bdeead40c611075107a8
Denis Benato <benato.denis96@gmail.com>
2024-12-31 15:42:33 +0100

    improv(main) do not lose the reference to the ctrl+C signal watcher

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ mod udev;
 mod watcher;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let log_level = match env::var("LOG_LEVEL") {
         Ok(value) => value,
         Err(_) => "info".to_string(),
@@ -30,17 +30,6 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     env_logger::init();
     const VERSION: &str = env!("CARGO_PKG_VERSION");
     log::info!("Starting InputPlumber v{}", VERSION);
-
-    // Setup CTRL+C handler
-    tokio::spawn(async move {
-        tokio::signal::ctrl_c().await.unwrap();
-        log::info!("Un-hiding all devices");
-        if let Err(e) = unhide_all().await {
-            log::error!("Unable to un-hide devices: {:?}", e);
-        }
-        log::info!("Shutting down");
-        process::exit(0);
-    });
 
     // Configure the DBus connection
     let connection = Connection::system().await?;
@@ -56,7 +45,17 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // Create an InputManager instance
     let mut input_manager = Manager::new(connection.clone());
 
-    let (input_man_result, request_name_result) = tokio::join!(
+    let (input_man_result, request_name_result, ctrl_c_result) = tokio::join!(
+        // Setup CTRL+C handler
+        tokio::spawn(async move {
+            tokio::signal::ctrl_c().await.unwrap();
+            log::info!("Un-hiding all devices");
+            if let Err(e) = unhide_all().await {
+                log::error!("Unable to un-hide devices: {:?}", e);
+            }
+            log::info!("Shutting down");
+            process::exit(0);
+        }),
         // Start the input manager and listen on DBus
         input_manager.run(),
         // Request the named bus
@@ -67,21 +66,31 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         Ok(_) => {
             log::info!("The input manager task has exited");
         }
-        Err(input_man_err) => {
-            log::error!("Error in joining the input manager task: {input_man_err}");
-            return Err(input_man_err);
+        Err(join_err) => {
+            log::error!("Error in joining the input manager task: {join_err}");
+            return Err(Box::new(join_err) as Box<dyn Error>);
         }
-    }
+    };
 
     match request_name_result {
         Ok(_) => {
             log::info!("The input manager task has exited");
         }
-        Err(request_name_err) => {
-            log::error!("Error in dbus request name operation: {request_name_err}");
-            return Err(Box::new(request_name_err));
+        Err(join_err) => {
+            log::error!("Error in joining dbus request name operation: {join_err}");
+            return Err(join_err as Box<dyn Error>);
         }
-    }
+    };
+
+    match ctrl_c_result {
+        Ok(_) => {
+            log::info!("The input manager task has exited");
+        }
+        Err(join_err) => {
+            log::error!("Error in joining ctrl+C watcher: {join_err}");
+            return Err(Box::new(join_err) as Box<dyn Error>);
+        }
+    };
 
     log::info!("InputPlumber stopped");
 
