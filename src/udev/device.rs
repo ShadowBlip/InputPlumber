dd0218117d3dc01ee229db48f334bd98f409124b
Denis Benato <benato.denis96@gmail.com>
2024-12-31 03:12:25 +0100

    feat(LED): LEDs initial implementation
    Enable acquisition of LED devices and setting of a fixed color if specified in the configuration.

--- src/udev/device.rs
+++ src/udev/device.rs
@@ -593,6 +593,9 @@ impl UdevDevice {
             "iio" => {
                 format!("iio://{}", self.sysname)
             }
+            "leds" => {
+                format!("leds://{}", self.sysname)
+            }
             _ => "".to_string(),
         }
     }
