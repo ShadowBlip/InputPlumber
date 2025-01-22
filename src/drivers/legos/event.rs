442e1eed81ffac43f2b27f0af28cf1d1929354a2
Derek J. Clark <derekjohn.clark@gmail.com>
2025-01-21 19:21:37 -0800

    feat(Legion Go S): Add IMU support - Adds reading IMU data from MCU interface 5. This device polls at 1000Hz and provides all 3 axis for each interial measurement in a single report. - While at it, performs cleanup of unused event types and remanes many others to be simpler. The more complex names aren't needed for Go S as it doesn't have as many interfaces as the Go. - Adds basic support for the touchpad. X/Y events are disabled until a firmware update can enable ABS values instead of REL values.

--- src/drivers/legos/event.rs
+++ src/drivers/legos/event.rs
@@ -1,12 +1,10 @@
 /// Events that can be emitted by the Legion Go controller
 #[derive(Clone, Debug)]
 pub enum Event {
-    GamepadButton(GamepadButtonEvent),
-    MouseButton(MouseButtonEvent),
-    TouchButton(TouchButtonEvent),
     Axis(AxisEvent),
+    Button(ButtonEvent),
+    Inertia(InertialEvent),
     Trigger(TriggerEvent),
-    Status(StatusEvent),
 }
 
 /// Binary input contain either pressed or unpressed
@@ -15,49 +13,9 @@ pub struct BinaryInput {
     pub pressed: bool,
 }
 
-/// Axis input contain (x, y) coordinates
-#[derive(Clone, Debug)]
-pub struct TouchAxisInput {
-    pub index: u8,
-    pub is_touching: bool,
-    pub x: u16,
-    pub y: u16,
-}
-
-/// Axis input contain (x, y) coordinates
-#[derive(Clone, Debug)]
-pub struct MouseAxisInput {
-    pub x: i16,
-    pub y: i16,
-}
-/// Axis input contain (x, y) coordinates
-#[derive(Clone, Debug)]
-pub struct JoyAxisInput {
-    pub x: i8,
-    pub y: i8,
-}
-
-// Status inputs contain some value that corresponds to the current status of a device.
-#[derive(Clone, Debug)]
-pub struct StatusInput {
-    pub value: u8,
-}
-
-/// Mouse Wheel contains negative integars
-#[derive(Clone, Debug)]
-pub struct MouseWheelInput {
-    pub value: i8,
-}
-
-/// Trigger input contains non-negative integars
-#[derive(Clone, Debug)]
-pub struct TriggerInput {
-    pub value: u8,
-}
-
 /// Button events represend binary inputs
 #[derive(Clone, Debug)]
-pub enum GamepadButtonEvent {
+pub enum ButtonEvent {
     /// A Button
     A(BinaryInput),
     /// X Button
@@ -88,55 +46,67 @@ pub enum GamepadButtonEvent {
     DTriggerL(BinaryInput),
     /// Z-axis button on the left stick
     ThumbL(BinaryInput),
-    /// Y1 on the back of the left gamepad
+    /// Y1 left back paddle
     Y1(BinaryInput),
-    /// Y2 on the back of the left gamepad
+    /// Y2 right back paddle
     Y2(BinaryInput),
-    /// Y3 on the back of the right gamepad
-    Y3(BinaryInput),
     /// Right shoulder button
     RB(BinaryInput),
     /// Binary sensor for right analog trigger
     DTriggerR(BinaryInput),
     /// Z-axis button on the right stick
     ThumbR(BinaryInput),
-    /// M2 on the side of the right controller
-    M2(BinaryInput),
-    /// M3 on the back of the right controller
-    M3(BinaryInput),
-    /// Mouse wheel click on the back of the right controller
-    MouseClick(BinaryInput),
+    /// Right touchpad click
+    //TODO: Wire this up with firmware update.
+    RPadPress(BinaryInput),
+    /// Right touchpad tapped
+    RPadTap(BinaryInput),
 }
 
-/// Button events represend binary inputs
+/// Axis input contain (x, y) coordinates
 #[derive(Clone, Debug)]
-pub enum MouseButtonEvent {
-    /// Y3 on the back of the right gamepad
-    Y3(BinaryInput),
-    /// M1 on the side of the right controller
-    M1(BinaryInput),
-    /// M2 on the side of the right controller
-    M2(BinaryInput),
-    /// M3 on the back of the right controller
-    M3(BinaryInput),
-    /// Mouse wheel click on the back of the right controller
-    Left(BinaryInput),
+pub struct TouchAxisInput {
+    pub index: u8,
+    pub is_touching: bool,
+    pub x: i8,
+    pub y: i8,
 }
 
-/// Button events represend binary inputs
+/// Axis input contain (x, y) coordinates
 #[derive(Clone, Debug)]
-pub enum TouchButtonEvent {
-    /// Tap to click button
-    Left(BinaryInput),
+pub struct JoyAxisInput {
+    pub x: i8,
+    pub y: i8,
 }
 
 /// Axis events are events that have (x, y) values
 #[derive(Clone, Debug)]
 pub enum AxisEvent {
+    //TODO: Wire this up with firmware update.
     Touchpad(TouchAxisInput),
     LStick(JoyAxisInput),
     RStick(JoyAxisInput),
-    Mouse(MouseAxisInput),
+}
+
+/// [InertialInput] represents the state of the IMU (x, y, z) values
+#[derive(Clone, Debug)]
+pub struct InertialInput {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+}
+
+/// [InertialEvent] has data from the IMU
+#[derive(Clone, Debug)]
+pub enum InertialEvent {
+    Accelerometer(InertialInput),
+    Gyro(InertialInput),
+}
+
+/// Trigger input contains non-negative integars
+#[derive(Clone, Debug)]
+pub struct TriggerInput {
+    pub value: u8,
 }
 
 /// Trigger events contain values indicating how far a trigger is pulled
@@ -144,16 +114,4 @@ pub enum AxisEvent {
 pub enum TriggerEvent {
     ATriggerL(TriggerInput),
     ATriggerR(TriggerInput),
-    MouseWheel(MouseWheelInput),
-}
-
-/// StatusEvent has data on if controllers are in FPS mode, connected, and the battery state.
-#[derive(Clone, Debug)]
-pub enum StatusEvent {
-    LeftControllerBattery(StatusInput),
-    LeftControllerMode0(StatusInput),
-    LeftControllerMode1(StatusInput),
-    RightControllerBattery(StatusInput),
-    RightControllerMode0(StatusInput),
-    RightControllerMode1(StatusInput),
 }
--- src/input/source/hidraw/legos.rs
+++ src/input/source/hidraw/legos.rs
@@ -7,8 +7,8 @@ use crate::{
     },
     input::{
         capability::{
-            Capability, Gamepad, GamepadAxis, GamepadButton, GamepadTrigger, Mouse, MouseButton,
-            Touch, TouchButton, Touchpad,
+            Capability, Gamepad, GamepadAxis, GamepadButton, GamepadTrigger, Touch, TouchButton,
+            Touchpad,
         },
         event::{native::NativeEvent, value::InputValue},
         source::{InputError, SourceInputDevice, SourceOutputDevice},
@@ -127,14 +127,6 @@ fn normalize_axis_value(event: event::AxisEvent) -> InputValue {
 
             InputValue::Vector2 { x, y }
         }
-        event::AxisEvent::Mouse(value) => {
-            let x = value.x as f64;
-            let x = Some(x);
-            let y = value.y as f64;
-            let y = Some(y);
-
-            InputValue::Vector2 { x, y }
-        }
     }
 }
 
@@ -150,10 +142,6 @@ fn normalize_trigger_value(event: event::TriggerEvent) -> InputValue {
             let max = driver::TRIGG_MAX;
             InputValue::Float(normalize_unsigned_value(value.value as f64, max))
         }
-        event::TriggerEvent::MouseWheel(value) => {
-            let max = driver::MOUSE_WHEEL_MAX;
-            InputValue::Float(normalize_unsigned_value(value.value as f64, max))
-        }
     }
 }
 
@@ -165,101 +153,93 @@ fn translate_events(events: Vec<event::Event>) -> Vec<NativeEvent> {
 /// Translate the given Legion Go event into a native event
 fn translate_event(event: event::Event) -> NativeEvent {
     match event {
-        event::Event::GamepadButton(button) => match button {
-            event::GamepadButtonEvent::A(value) => NativeEvent::new(
+        event::Event::Button(button) => match button {
+            event::ButtonEvent::A(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::South)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::X(value) => NativeEvent::new(
+            event::ButtonEvent::X(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::North)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::B(value) => NativeEvent::new(
+            event::ButtonEvent::B(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::East)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::Y(value) => NativeEvent::new(
+            event::ButtonEvent::Y(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::West)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::Menu(value) => NativeEvent::new(
+            event::ButtonEvent::Menu(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::Start)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::View(value) => NativeEvent::new(
+            event::ButtonEvent::View(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::Select)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::Legion(value) => NativeEvent::new(
+            event::ButtonEvent::Legion(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::Guide)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::QuickAccess(value) => NativeEvent::new(
+            event::ButtonEvent::QuickAccess(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::QuickAccess)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::DPadDown(value) => NativeEvent::new(
+            event::ButtonEvent::DPadDown(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::DPadDown)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::DPadUp(value) => NativeEvent::new(
+            event::ButtonEvent::DPadUp(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::DPadUp)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::DPadLeft(value) => NativeEvent::new(
+            event::ButtonEvent::DPadLeft(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::DPadLeft)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::DPadRight(value) => NativeEvent::new(
+            event::ButtonEvent::DPadRight(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::DPadRight)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::LB(value) => NativeEvent::new(
+            event::ButtonEvent::LB(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::LeftBumper)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::DTriggerL(value) => NativeEvent::new(
+            event::ButtonEvent::DTriggerL(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::LeftTrigger)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::ThumbL(value) => NativeEvent::new(
+            event::ButtonEvent::ThumbL(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::LeftStick)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::Y1(value) => NativeEvent::new(
+            event::ButtonEvent::Y1(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::LeftPaddle1)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::Y2(value) => NativeEvent::new(
-                Capability::Gamepad(Gamepad::Button(GamepadButton::LeftPaddle2)),
-                InputValue::Bool(value.pressed),
-            ),
-            event::GamepadButtonEvent::RB(value) => NativeEvent::new(
+            event::ButtonEvent::RB(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::RightBumper)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::DTriggerR(value) => NativeEvent::new(
+            event::ButtonEvent::DTriggerR(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::RightTrigger)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::ThumbR(value) => NativeEvent::new(
+            event::ButtonEvent::ThumbR(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::RightStick)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::Y3(value) => NativeEvent::new(
+            event::ButtonEvent::Y2(value) => NativeEvent::new(
                 Capability::Gamepad(Gamepad::Button(GamepadButton::RightPaddle1)),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::M3(value) => NativeEvent::new(
-                Capability::Gamepad(Gamepad::Button(GamepadButton::RightPaddle2)),
+            event::ButtonEvent::RPadPress(value) => NativeEvent::new(
+                Capability::Touchpad(Touchpad::RightPad(Touch::Button(TouchButton::Press))),
                 InputValue::Bool(value.pressed),
             ),
-            event::GamepadButtonEvent::M2(value) => NativeEvent::new(
-                Capability::Gamepad(Gamepad::Button(GamepadButton::RightPaddle3)),
-                InputValue::Bool(value.pressed),
-            ),
-            event::GamepadButtonEvent::MouseClick(value) => NativeEvent::new(
-                Capability::Mouse(Mouse::Button(MouseButton::Middle)),
+            event::ButtonEvent::RPadTap(value) => NativeEvent::new(
+                Capability::Touchpad(Touchpad::RightPad(Touch::Button(TouchButton::Press))),
                 InputValue::Bool(value.pressed),
             ),
         },
@@ -276,9 +256,6 @@ fn translate_event(event: event::Event) -> NativeEvent {
                 Capability::Gamepad(Gamepad::Axis(GamepadAxis::RightStick)),
                 normalize_axis_value(axis),
             ),
-            event::AxisEvent::Mouse(_) => {
-                NativeEvent::new(Capability::Mouse(Mouse::Motion), normalize_axis_value(axis))
-            }
         },
         event::Event::Trigger(trigg) => match trigg.clone() {
             event::TriggerEvent::ATriggerL(_) => NativeEvent::new(
@@ -289,45 +266,31 @@ fn translate_event(event: event::Event) -> NativeEvent {
                 Capability::Gamepad(Gamepad::Trigger(GamepadTrigger::RightTrigger)),
                 normalize_trigger_value(trigg),
             ),
-            event::TriggerEvent::MouseWheel(_) => {
-                NativeEvent::new(Capability::NotImplemented, InputValue::Bool(false))
-            }
         },
-        event::Event::MouseButton(button) => match button {
-            event::MouseButtonEvent::Y3(value) => NativeEvent::new(
-                Capability::Mouse(Mouse::Button(MouseButton::Extra)),
-                InputValue::Bool(value.pressed),
+        event::Event::Inertia(motion) => match motion {
+            event::InertialEvent::Accelerometer(value) => NativeEvent::new(
+                Capability::Gamepad(Gamepad::Accelerometer),
+                InputValue::Vector3 {
+                    x: Some(value.x as f64),
+                    y: Some(value.y as f64),
+                    z: Some(value.z as f64),
+                },
             ),
-            event::MouseButtonEvent::M1(value) => NativeEvent::new(
-                Capability::Mouse(Mouse::Button(MouseButton::Left)),
-                InputValue::Bool(value.pressed),
-            ),
-            event::MouseButtonEvent::M2(value) => NativeEvent::new(
-                Capability::Mouse(Mouse::Button(MouseButton::Right)),
-                InputValue::Bool(value.pressed),
-            ),
-            event::MouseButtonEvent::M3(value) => NativeEvent::new(
-                Capability::Mouse(Mouse::Button(MouseButton::Side)),
-                InputValue::Bool(value.pressed),
-            ),
-            event::MouseButtonEvent::Left(value) => NativeEvent::new(
-                Capability::Mouse(Mouse::Button(MouseButton::Middle)),
-                InputValue::Bool(value.pressed),
+            event::InertialEvent::Gyro(value) => NativeEvent::new(
+                Capability::Gamepad(Gamepad::Gyro),
+                InputValue::Vector3 {
+                    x: Some(value.x as f64),
+                    y: Some(value.y as f64),
+                    z: Some(value.z as f64),
+                },
             ),
         },
-        event::Event::TouchButton(button) => match button {
-            event::TouchButtonEvent::Left(value) => NativeEvent::new(
-                Capability::Touchpad(Touchpad::RightPad(Touch::Button(TouchButton::Press))),
-                InputValue::Bool(value.pressed),
-            ),
-        },
-
-        _ => NativeEvent::new(Capability::NotImplemented, InputValue::Bool(false)),
     }
 }
 
 /// List of all capabilities that the Legion Go driver implements
 pub const CAPABILITIES: &[Capability] = &[
+    Capability::Gamepad(Gamepad::Accelerometer),
     Capability::Gamepad(Gamepad::Axis(GamepadAxis::LeftStick)),
     Capability::Gamepad(Gamepad::Axis(GamepadAxis::RightStick)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::DPadDown)),
@@ -338,29 +301,22 @@ pub const CAPABILITIES: &[Capability] = &[
     Capability::Gamepad(Gamepad::Button(GamepadButton::Guide)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::LeftBumper)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::LeftPaddle1)),
-    Capability::Gamepad(Gamepad::Button(GamepadButton::LeftPaddle2)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::LeftStick)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::LeftTrigger)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::North)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::QuickAccess)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::RightBumper)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::RightPaddle1)),
-    Capability::Gamepad(Gamepad::Button(GamepadButton::RightPaddle2)),
-    Capability::Gamepad(Gamepad::Button(GamepadButton::RightPaddle3)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::RightStick)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::RightTrigger)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::Select)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::South)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::Start)),
     Capability::Gamepad(Gamepad::Button(GamepadButton::West)),
+    Capability::Gamepad(Gamepad::Gyro),
     Capability::Gamepad(Gamepad::Trigger(GamepadTrigger::LeftTrigger)),
     Capability::Gamepad(Gamepad::Trigger(GamepadTrigger::RightTrigger)),
-    Capability::Mouse(Mouse::Button(MouseButton::Extra)),
-    Capability::Mouse(Mouse::Button(MouseButton::Left)),
-    Capability::Mouse(Mouse::Button(MouseButton::Middle)),
-    Capability::Mouse(Mouse::Button(MouseButton::Right)),
-    Capability::Mouse(Mouse::Button(MouseButton::Side)),
-    Capability::Mouse(Mouse::Motion),
     Capability::Touchpad(Touchpad::RightPad(Touch::Button(TouchButton::Press))),
+    Capability::Touchpad(Touchpad::RightPad(Touch::Button(TouchButton::Touch))),
     Capability::Touchpad(Touchpad::RightPad(Touch::Motion)),
 ];
