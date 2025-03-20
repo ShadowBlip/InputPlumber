d8022288ef48ba1c362844afc635a041775693f8
Derek J. Clark <derekjohn.clark@gmail.com>
2025-03-20 13:12:03 -0700

    feat(Legion Go S): Add touchpad support - Adds touchpad support for the Legion Go S.

--- src/drivers/legos/hid_report.rs
+++ src/drivers/legos/hid_report.rs
@@ -32,70 +32,78 @@ impl OutputReportType {
 #[derive(PackedStruct, Debug, Copy, Clone, PartialEq)]
 #[packed_struct(bit_numbering = "msb0", size_bytes = "32")]
 pub struct XInputDataReport {
-    #[packed_field(bits = "23")]
-    pub y1: bool,
-    #[packed_field(bits = "22")]
-    pub y2: bool,
-    #[packed_field(bits = "17")]
-    pub view: bool,
-    #[packed_field(bits = "16")]
-    pub menu: bool,
-    #[packed_field(bits = "15")]
-    pub a: bool,
-    #[packed_field(bits = "14")]
-    pub b: bool,
-    #[packed_field(bits = "13")]
-    pub x: bool,
-    #[packed_field(bits = "12")]
-    pub y: bool,
-    #[packed_field(bits = "11")]
-    pub lb: bool,
-    #[packed_field(bits = "10")]
-    pub d_trigger_l: bool,
-    #[packed_field(bits = "9")]
-    pub rb: bool,
-    #[packed_field(bits = "8")]
-    pub d_trigger_r: bool,
-    #[packed_field(bits = "7")]
-    pub legion: bool,
-    #[packed_field(bits = "6")]
-    pub quick_access: bool,
-    #[packed_field(bits = "5")]
-    pub thumb_l: bool,
-    #[packed_field(bits = "4")]
-    pub thumb_r: bool,
-    #[packed_field(bits = "3")]
-    pub up: bool,
-    #[packed_field(bits = "2")]
-    pub down: bool,
-    #[packed_field(bits = "1")]
-    pub left: bool,
+    // byte 0
     #[packed_field(bits = "0")]
     pub right: bool,
-
+    #[packed_field(bits = "1")]
+    pub left: bool,
+    #[packed_field(bits = "2")]
+    pub down: bool,
+    #[packed_field(bits = "3")]
+    pub up: bool,
+    #[packed_field(bits = "4")]
+    pub thumb_r: bool,
+    #[packed_field(bits = "5")]
+    pub thumb_l: bool,
+    #[packed_field(bits = "6")]
+    pub quick_access: bool,
+    #[packed_field(bits = "7")]
+    pub legion: bool,
+    // byte 1
+    #[packed_field(bits = "8")]
+    pub d_trigger_r: bool,
+    #[packed_field(bits = "9")]
+    pub rb: bool,
+    #[packed_field(bits = "10")]
+    pub d_trigger_l: bool,
+    #[packed_field(bits = "11")]
+    pub lb: bool,
+    #[packed_field(bits = "12")]
+    pub y: bool,
+    #[packed_field(bits = "13")]
+    pub x: bool,
+    #[packed_field(bits = "14")]
+    pub b: bool,
+    #[packed_field(bits = "15")]
+    pub a: bool,
+    // byte 2
+    #[packed_field(bits = "16")]
+    pub menu: bool,
+    #[packed_field(bits = "17")]
+    pub view: bool,
+    //byte 3
+    #[packed_field(bits = "22")]
+    pub y2: bool,
+    #[packed_field(bits = "23")]
+    pub y1: bool,
+    // byte 4
     #[packed_field(bytes = "4")]
     pub l_stick_x: i8,
+    // byte 5
     #[packed_field(bytes = "5")]
     pub l_stick_y: i8,
+    // byte 6
     #[packed_field(bytes = "6")]
     pub r_stick_x: i8,
+    // byte 7
     #[packed_field(bytes = "7")]
     pub r_stick_y: i8,
-
     // byte 8
-    #[packed_field(bits = "71")]
+    #[packed_field(bits = "71")] // Deprecated
     pub rpad_touching: bool,
     // byte 9
-    #[packed_field(bits = "79")]
+    #[packed_field(bits = "79")] // Deprecated
     pub rpad_tap: bool,
-
+    // byte 10
     #[packed_field(bytes = "10")]
     pub touch_x: i8,
+    // byte 11
     #[packed_field(bytes = "11")]
     pub touch_y: i8,
-
+    // byte 12
     #[packed_field(bytes = "12")]
     pub a_trigger_l: u8,
+    // byte 13
     #[packed_field(bytes = "13")]
     pub a_trigger_r: u8,
 }
@@ -159,6 +167,42 @@ pub struct InertialInputDataReport {
     pub z: Integer<i16, packed_bits::Bits<16>>,
 }
 
+//TouchData
+#[derive(PackedStruct, Debug, Copy, Clone, PartialEq)]
+#[packed_struct(bit_numbering = "msb0", size_bytes = "10")]
+pub struct TouchInputDataReport {
+    // byte 0
+    #[packed_field(bytes = "0")]
+    pub report_id: u8,
+    // byte 1
+    #[packed_field(bits = "8..11")]
+    pub contact_id: u8,
+    #[packed_field(bits = "14")]
+    pub tip_switch: bool,
+    #[packed_field(bits = "15")]
+    pub confidence: bool,
+    // byte 2
+    #[packed_field(bytes = "2")]
+    pub touch_x: i8,
+    // byte 3
+    #[packed_field(bytes = "3")]
+    pub unk_3: u8,
+    // byte 4
+    #[packed_field(bytes = "4")]
+    pub touch_y: i8,
+    // byte 5
+    #[packed_field(bytes = "5")]
+    pub unk_5: u8,
+    // bytes 6-7
+    #[packed_field(bytes = "6..=7", endian = "lsb")]
+    pub scan_time: Integer<i16, packed_bits::Bits<16>>,
+    // byte 8
+    #[packed_field(bytes = "8")]
+    pub contact_count: u8,
+    // byte 9
+    #[packed_field(bits = "72")]
+    pub press: u8,
+}
 #[derive(PackedStruct, Debug, Copy, Clone, PartialEq)]
 #[packed_struct(bit_numbering = "msb0", size_bytes = "9")]
 pub struct RumbleOutputDataReport {
