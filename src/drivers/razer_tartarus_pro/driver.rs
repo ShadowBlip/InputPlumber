69e5ca73b2d4bcaa5b2f2b5aa0a94c66d0c95dcb
Joseph East <eastyjr@gmail.com>
2026-02-05 22:01:02 +1030

    Initial functional implementation for keyboard and Dpad. Scroll wheel and analog mode are not yet working.

--- src/drivers/razer_tartarus_pro/driver.rs
+++ src/drivers/razer_tartarus_pro/driver.rs
@@ -1,4 +1,4 @@
-use super::event::Event;
+use super::event::{Event, KeyCodes};
 use crate::udev::device::UdevDevice;
 use hidapi::HidDevice;
 use std::{error::Error, ffi::CString};
@@ -13,7 +13,13 @@ const RAZER_FEATURE_ENDPOINT: i32 = 2;
 pub struct Driver {
     device: HidDevice,
     razer_message_id: u8,
+    analog_state: [u8; MESSAGE_DATA_PAYLOAD],
+    key_state: Vec<KeyCodes>,
 }
+
+/// This driver implementation is shared across all three HID handles on the
+/// Tartarus Pro. Certain code-paths will only execute on certain handles.
+/// Refer to handle_input_report()
 
 impl Driver {
     pub fn new(udevice: UdevDevice) -> Result<Self, Box<dyn Error + Send + Sync>> {
@@ -39,20 +45,25 @@ impl Driver {
         Ok(Self {
             device,
             razer_message_id,
+            analog_state: [0; MESSAGE_DATA_PAYLOAD],
+            key_state: Vec::new(),
         })
     }
 
     pub fn poll(&mut self) -> Result<Vec<Event>, Box<dyn Error + Send + Sync>> {
-        let events = Vec::new();
+        let mut buf = [0; MESSAGE_DATA_PAYLOAD];
+        let bytes_read = self.device.read(&mut buf[..])?;
+        let slice = &buf[..bytes_read];
+        let events = self.handle_input_report(slice, bytes_read)?;
         Ok(events)
     }
 
-    // Rust's USBHID implementation enforces the use of Windows-ism of needing a dud zeroth byte
-    // for unused report ID, so +1 the buffer size. 91 now for Razer, our window is [1..91].
-    // Ignore [0]. The XOR is always the penultimate byte with the final byte being 0.
-    // The first byte is a status byte which for PC->DEV is 0. Replies always start with 0x2.
-    // The next 4 bytes are used in other Razer devices, but not the Tartarus so we leave them 0.
-
+    // The hidapi crate enforces the Windows-ism of needing a dud zeroth byte for sending feature
+    // reports to devices with no report ID specified. For unused report ID This means our window
+    // is [1..] and we ignore [0], though leave it 0. The XOR is always the penultimate byte with
+    // the ultimate byte being 0. Byte [1] is a status byte which for PC->DEV is 0. Replies
+    // always start with 0x2. Bytes [3..5] are used in other Razer devices but not the Tartarus
+    // so we leave them 0.
     fn razer_report(
         cmd_class: i32,
         cmd_id: i32,
@@ -108,19 +119,103 @@ impl Driver {
         }
     }
 
-    // The Tartarus Pro utilises a write/read cycle so there are no discrete
-    // read and write operations, just a transaction and a set of expectations.
     fn transaction(
         packet: [u8; RAZER_HID_ARRAY_SIZE],
         handle: &HidDevice,
     ) -> Result<Vec<u8>, Box<dyn Error + Send + Sync>> {
         if handle.get_device_info().unwrap().interface_number() == RAZER_FEATURE_ENDPOINT {
-            let res = handle.send_feature_report(&packet);
+            let _ = handle.send_feature_report(&packet);
             let mut feature_buffer: [u8; RAZER_HID_ARRAY_SIZE] = [0; RAZER_HID_ARRAY_SIZE];
             let _ = handle.get_feature_report(&mut feature_buffer);
-            return Ok(Self::razer_decapsulate(&packet, &feature_buffer)?)
+            return Ok(Self::razer_decapsulate(&packet, &feature_buffer)?);
         }
         log::error!("Tartarus attempting to transact on incorrect endpoint");
         return Ok(Vec::new());
+    }
+
+    fn handle_input_report(
+        &mut self,
+        buf: &[u8],
+        bytes_read: usize,
+    ) -> Result<Vec<Event>, Box<dyn Error + Send + Sync>> {
+        let info = self.device.get_device_info()?;
+        let mut events = Vec::new();
+
+        // Depending on what handle we actually are (this code runs on all 3)
+        // we have different reports that we can expect and respond to in kind.
+
+        // Endpoint 1.1 is always played as per its HID report descriptor but it is simple enough
+        // to re-implement here. There is no report ID, it gets straight to business.
+        // This interface captures the D-pad and the aux key.
+
+        // Endpoint 1.2 has two personalities, if report ID 1 is seen then it is in default
+        // keyboard mode and is played exactly as its descriptor says. If report ID 6 is seen
+        // that is analog mode so we need to run the threshold checks.
+        // This interface regardless of personality captures the 20 numbered keys.
+
+        // Endpoint 1.3 is in the same basket as 1.1, different patterns to match though.
+        // This interface captures the scroll wheel.
+
+        match info.interface_number() {
+            0 => self.handle_basic(buf, KeyCodes::Aux, false),
+            1 => {
+                match buf[0] {
+                    0x1 => return self.handle_basic(buf, KeyCodes::Blank, true),
+                    0x6 => {
+                        // Perform thresholding
+                        todo!()
+                    }
+                    _ => {
+                        // Other report types exist but don't appear to be
+                        // actually used.
+                        Ok(events)
+                    }
+                }
+            }
+            2 => self.handle_basic(buf, KeyCodes::MClick, false),
+            _ => Ok(events),
+        }
+    }
+
+    // The basic case for all 3 endpoints is essentially identical. The first byte is unique for
+    // each; For endpoints 1.1 and 1.3 this is where code 0x04 is interpreted and discarded for
+    // endpoint 1.2 as it was a report ID which has served its purpose if we got this far.
+    // Given the conversion to variant space it is open to misinterpretation if left there.
+    fn handle_basic(
+        &mut self,
+        buf: &[u8],
+        key_replace: KeyCodes,
+        overwrite: bool,
+    ) -> Result<Vec<Event>, Box<dyn Error + Send + Sync>> {
+        let mut events = Vec::new();
+        let mut pad_state: Vec<KeyCodes> = buf.iter().map(|&s| KeyCodes::from(s)).collect();
+
+        // Override Byte 0 as specified and remove any blanks
+        if pad_state[0] == KeyCodes::KeyTwelve || overwrite {
+            pad_state[0] = key_replace;
+        }
+        pad_state.retain(|x| *x != KeyCodes::Blank);
+
+        // If a key is present in the report then it was pressed
+        for i in &pad_state {
+            events.push(Event {
+                key: i.clone(),
+                pressed: true,
+            });
+        }
+
+        // If a key is missing compared to last time then indicate it is no longer pressed
+        for i in &self.key_state {
+            if !pad_state.contains(&i) {
+                events.push(Event {
+                    key: i.clone(),
+                    pressed: false,
+                });
+            }
+        }
+
+        // Save state for next time
+        self.key_state = pad_state;
+        Ok(events)
     }
 }
