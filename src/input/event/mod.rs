403d3ea5eeb5b6264f6572af28d5415a231c9083
Nilesh Chakraborty <nileshchakraborty@gmail.com>
2026-09-12 11:18:35 -0700

    test(event): move input value unit tests to value_test.rs

--- src/input/event/mod.rs
+++ src/input/event/mod.rs
@@ -4,6 +4,9 @@ pub mod evdev;
 pub mod native;
 pub mod value;
 
+#[cfg(test)]
+pub mod value_test;
+
 /// Events are events that flow from source devices to target devices
 /// TODO: Remove this enum in favor of directly using NativeEvent
 #[derive(Debug, Clone)]
